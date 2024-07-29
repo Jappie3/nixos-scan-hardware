500284f445f709206945b8933b5e08fe83e6a6ce
Jappie3 <jasper22034@gmail.com>
2024-07-29 16:56:12 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+      ]
+      (system: function nixpkgs.legacyPackages.${system});
+  in {
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          perl
+          systemd # for systemd-detect-virt
+          btrfs-progs # for btrfs
+        ];
+      };
+    });
+  };
+}
